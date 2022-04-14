3e8a6de828bdd50b1fa752e0a5a5401704e2168b
Mario Kamberi <mario_kamberi@hotmail.com>
2022-04-14 16:13:15 +0200

    methods

---
+++ src/methods/HelloWorld.java
@@ -0,0 +1,29 @@
+package methods;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        hello();
+        world();
+    }
+
+    public static void hello(){
+        //WRITE YOUR CODE HERE
+        System.out.print("Hello ");
+
+    }
+
+    public static void world(){
+        //WRITE YOUR CODE HERE
+        System.out.print("World!");
+
+    }
+
+}
+/*
+Given 2 static methods hello() and world().
+Provide an implementation for both of them in order to print appropriate message.
+So hello() method should print "Hello" and world() method should print "World!".`
+
+output: Hello World!
+
+ */
